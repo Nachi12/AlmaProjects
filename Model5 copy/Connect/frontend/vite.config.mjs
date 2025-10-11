e463ff507a21f01fbb2cbdc0fc5f38e3d8c6570c
Nachiketa NR <nrnachi34@example.com>
2025-10-11 19:01:20 +0530

    Fix vercel crash (removed secret)

---
+++ Model5 copy/Connect/frontend/vite.config.mjs
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
+export default defineConfig({
+   base: "./",
+
+  server: {
+    host: "localhost",
+    port: 5173,
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve("./src"),
+    },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/tests/setup.js",
+    css: true,
+    include: ["src/**/*.test.{js,jsx,ts,tsx}"],
+    exclude: ["backend/**/*", "node_modules/**/*", "dist/**/*"],
+  },
+});
